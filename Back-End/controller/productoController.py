36fc1a52c22d3a6d44537e36639643f3ed78c40b
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-05-27 00:48:47 -0600

    Funciona dar de alta productos

--- Back-End/controller/productoController.py
+++ Back-End/controller/productoController.py
@@ -25,23 +25,14 @@ def darDeAltaProducto():
     precioC = request.form['precioC']
     cantidad = request.form['cantidad']
     idVendedor = request.form['id']
-
-    precio=precioFloat(precioP, precioC)
-    print(precio)
-
+    precio=precioFloat(precioP, precioC) ## Pasamos los dos enteros a flotante
     if allowed_file(imagen.filename):
-        memoria=io.BytesIO()
-        imagen.save(memoria)
-        memoria.seek(0)
-        blob=memoria.read()
-        producto = Producto(nombre, descripcion, precio, blob,cantidad, categoria, idVendedor)
+        blob=obtenerBinario(imagen) ## Pasamos la imagen a binario
+        producto = Producto(nombre, descripcion, precio, blob ,cantidad, categoria, idVendedor)
         db.session.add(producto)
         db.session.commit()
 
     else:
-        flash('Formato de imagen no permitido', 'danger')
-        return redirect(url_for('alta_producto'))
+        {'mensaje': 'Formato de archivo no permitido'}
 
-    
-
-    return "hola"
+    return {'mensaje': 'Producto dado de alta exitosamente'}
--- Back-End/model/modelProducto.py
+++ Back-End/model/modelProducto.py
@@ -1,7 +1,20 @@
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+import io
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'} ##Para ser capaces de traducir despues la imagen
 
 def allowed_file(filename):
+    """Funcion que nos permite saber si el archivo que se sube es una imagen"""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def precioFloat(precioP, precioC):
+    """Funcion que toma dos enteros, uno representado el entero y el otro la parte flotante
+        y los convierte en un flotante"""
     return float(precioP) + float(precioC) / 100
+
+def obtenerBinario(imagen):
+    """ Funcion que dado un FileStorage de una imagen, la convierte en un binario"""
+    memoria=io.BytesIO()
+    imagen.save(memoria)
+    memoria.seek(0)
+    return memoria.read()
+
