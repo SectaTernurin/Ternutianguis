198d0d0bdca5ec552a8070d6808edd225f6d9b61
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-05-29 19:21:27 -0600

    Se corrige error de registro

--- Back-End/model/modelComprador.py
+++ Back-End/model/modelComprador.py
@@ -6,6 +6,7 @@ from alchemyClasses.Comprador import Comprador
 def verificacionInformacion(correo, contrasena):
     tError = False
     compradorValido = Comprador.query.filter_by(correo = correo).first()
+    print(compradorValido)
     if compradorValido != None:
         if compradorValido.contrasena != contrasena:
             tError = True
