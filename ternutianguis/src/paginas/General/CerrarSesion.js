939c051d6355e4678478802b9ca5509af33bce38
dulcemora <dulcemora03@ciencias.unam.mx>
2024-05-30 21:27:31 -0500

    Agregando cookies para cerrar sesion

--- ternutianguis/src/paginas/General/CerrarSesion.js
+++ ternutianguis/src/paginas/General/CerrarSesion.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './CerrarSesion.css';
+import { Link, useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 import Image from 'react-bootstrap/Image';
-import Cookies from 'js-cookie'; 
 
 export const CerrarSesion = () => {
+  const navigate = useNavigate();
 
   const cerrarSesion = () => {
     console.log('Cerrando sesión');
     Cookies.remove('usuario');
-  }
+    navigate('/'); // Redirige a la página /home después de cerrar sesión
+  };
 
   return (
     <div className="logout-container">
       <div className="left-section">
         <div className="image-container">
           <h1 className="title">Ternutianguis</h1>
-          <Image src={require('../imagenes/ternurin.png')} alt="Ternutiaguis" />
+          <Image src={require('../imagenes/ternurin.png')} alt="Ternutianguis" />
         </div>
       </div>
       <div className="right-section">
-        <h1>¿Esta seguro de cerrar sesión?</h1>
+        <h1>¿Está seguro de cerrar sesión?</h1>
         <div className="buttons">
-          <Link to="/" className="accept-button" onClick={cerrarSesion}>Aceptar</Link>
+          <button className="accept-button" onClick={cerrarSesion}>Aceptar</button>
           <Link to="/inicio" className="cancel-button">Cancelar</Link>
         </div>
       </div>
@@ -30,4 +31,4 @@ export const CerrarSesion = () => {
   );
 };
 
-//export default Logout;
+export default CerrarSesion;
